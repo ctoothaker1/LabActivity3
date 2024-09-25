46cf8beb78a7d7ead9428fd4b47968a18aed74f7
ctoothaker1 <ctoothaker1@gmail.com>
2024-09-25 16:05:44 -0400

    Created feature-1, when edittext is blank, displaytextview will ask user to enter a name

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.helloworld
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,7 +22,17 @@ class MainActivity : AppCompatActivity() {
 
         
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            //displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+
+            val name = findViewById<EditText>(R.id.nameEditText).text.toString().trim()
+
+            if (name.isEmpty())
+                displayTextView.text = "Please enter your name"
+
+            else {
+                displayTextView.text = "Hello, $name"
+            }
+
         }
 
 
